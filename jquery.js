977a5178313fcb52a7f5c4646d7a76e89221f798
Katre123 <45009439+Katre123@users.noreply.github.com>
2019-03-21 16:02:34 +0200

    Jquery harjutus
    Payed / Unpayed / Pending

---
+++ jquery.js
@@ -0,0 +1,14 @@
+
+ $(document).ready(function(){
+
+    $("button").click(function () {
+	$('li').find('.unpayed').css('background','red');
+	 $("button").click(function () {
+	$('li').find('.payed').css('background','yellow');
+	 $("button").click(function () {
+	$('li').find('.pending').css('background','green');			
+    });
+	 });
+	  });
+ });
+	
